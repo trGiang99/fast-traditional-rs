0290b9945162472ef49b0405efb21bcb4cdfd475
trGiang99 <dtrgiang.1299@gmail.com>
2021-07-13 22:38:45 +0700

    Separate baseline methods out of kNN helpers

--- knn/knn_helper.py
+++ knn/knn_helper.py
@@ -3,68 +3,6 @@ from numba import njit
 
 
 @njit
-def _baseline_sgd(X, global_mean, n_x, n_y, n_epochs=20, lr=0.005, reg=0.02):
-    """Optimize biases using SGD.
-    Args:
-        X (ndarray): the training set with size (|TRAINSET|, 3)
-        global_mean (float): mean ratings in training set
-        n_x (np.array): number of users
-        n_y (np.array): number of items
-        n_epochs (int): number of iterations to train
-        lr (float): the learning rate
-        reg (float): the regularization strength
-    Returns:
-        A tuple ``(bx, by)``, which are users and items baselines.
-    """
-
-    bx = np.zeros(n_x)
-    by = np.zeros(n_y)
-
-    for _ in range(n_epochs):
-        for i in range(X.shape[0]):
-            x, y, rating = int(X[i, 0]), int(X[i, 1]), X[i, 2]
-            err = (rating - (global_mean + bx[x] + by[y]))
-            bx[x] += lr * (err - reg * bx[x])
-            by[y] += lr * (err - reg * by[y])
-
-    return bx, by
-
-
-@njit
-def _baseline_als(global_mean, n_x, n_y, x_rated, y_ratedby, n_epochs=10, reg_x=15, reg_y=10):
-    """Optimize biases using ALS.
-    Args:
-        global_mean (float): mean ratings in training set
-        n_x (np.array): number of users
-        n_y (np.array): number of items
-        n_epochs (int): number of iterations to train
-        reg_x (float): regularization term for vector bx
-        reg_y (float): regularization term for vector by
-    Returns:
-        A tuple ``(bx, by)``, which are users and items baselines.
-    """
-
-    bx = np.zeros(n_x)
-    by = np.zeros(n_y)
-
-    for _ in range(n_epochs):
-        for y in range(n_y):
-            dev_y = 0
-            for x, r in x_rated[y]:
-                dev_y += r - global_mean - bx[int(x)]
-
-            by[y] = dev_y / (reg_y + x_rated[y].shape[0])
-
-        for x in range(n_x):
-            dev_x = 0
-            for y, r in y_ratedby[x]:
-                dev_x += r - global_mean - by[int(y)]
-            bx[x] = dev_x / (reg_x + y_ratedby[x].shape[0])
-
-    return bx, by
-
-
-@njit
 def _predict(x_id, y_id, y_rated, S, k, k_min):
     """Predict rating of user x for item y (if iiCF).
     Args:
